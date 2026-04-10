52ba65f35b65703a305ecf708fb540c02c6d6792
Ricarditti <ricarditti5@gmail.com>
2026-04-10 16:04:13 +0100

    Testei o type convertion

--- Go/hello.go
+++ Go/hello.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	name := "Ricardo"
 	fmt.Println("Hello, ", name+"!")
+	var i int32 = 1000
+	var j int64 = int64(i) // Convert int32 to int64
+	fmt.Println(j)
+
+	var k int8 = int8(i) // Convert int32 to int8 (potential data loss)
+	fmt.Println(k)       // Output: -24 (due to overflow)
 }
