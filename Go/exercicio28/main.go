442d25f807ee58cd15e2377d79944c9a289216c5
Ricarditti <ricarditti5@gmail.com>
2026-04-21 20:25:31 +0100

    .

--- Go/exercicio28/main.go
+++ Go/exercicio28/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main(){
-	
+func main() {
+	nota := 2
+
+	switch {
+	case nota >= 0 && nota <= 56:
+		fmt.Println("F")
+	case nota >= 60 && nota <= 69:
+		fmt.Println("D")
+	case nota >= 70 && nota <= 79:
+		fmt.Println("C")
+	case nota >= 80 && nota <= 89:
+		fmt.Println("B")
+	case nota >= 90 && nota <= 100:
+		fmt.Println("A")
+	}
 }
