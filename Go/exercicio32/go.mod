1b70c890b3529e8adc132bf585692475854f58cf
Ricarditti <ricarditti5@gmail.com>
2026-04-26 19:27:49 +0100

    .

---
+++ Go/exercicio32/go.mod
@@ -0,0 +1,3 @@
+module exercicio32
+
+go 1.26.2
---
+++ Go/exercicio32/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 1, 3, 7, 5, 10, 7, 2, 8}
+
+	for _, num := range numbers {
+		if num%2 == 0 {
+			break
+		}
+
+		fmt.Println(num)
+	}
+}
