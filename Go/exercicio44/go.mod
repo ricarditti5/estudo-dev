d8215aa001b5d5869c868ab627fa7591df4dcc3f
Ricarditti <ricarditti5@gmail.com>
2026-05-02 20:47:42 +0100

    .

---
+++ Go/exercicio44/go.mod
@@ -0,0 +1,3 @@
+module exercicio44
+
+go 1.25.0
---
+++ Go/exercicio44/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func printMemory() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	fmt.Printf("\nUsing memory %v", m.Alloc/1024/1024)
+}
+
+func main() {
+
+	printMemory()
+	memoryAloc := make([]byte, 100*1024*1024)
+	printMemory()
+	//memoryAloc = nil
+	//runtime.GC()
+	fmt.Println("\n", len(memoryAloc))
+}
